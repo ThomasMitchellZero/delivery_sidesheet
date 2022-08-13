3916e5215e78656f2db2790f74ef3b5e48ee54e5
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-08-13 18:18:26 -0400

    adding in button styling

--- src/components/Action_Menu/StartCancel.js
+++ src/components/Action_Menu/StartCancel.js
@@ -4,7 +4,9 @@ import Button from "../UI/Button";
 const StartCancel = (props) => {
   return (
     <ButtonContainer>
-      <Button></Button>
+      <Button buttonType="tertiary__disabled">Tertiary</Button>
+      <Button buttonType="secondary__disabled">2nd</Button>
+      <Button buttonType="primary__disabled">Cancel Delivery</Button>
     </ButtonContainer>
   );
 };
