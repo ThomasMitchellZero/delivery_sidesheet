a1c912172c60cf463419a1a2b591961bd7f84185
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-08-13 17:25:56 -0400

    Adding in containers for the different chip statuses

---
+++ src/components/Action_Menu/DeliveryStatus.js
@@ -0,0 +1,13 @@
+import StartCancel from "./StartCancel";
+import StatusChips from "./StatusChips";
+
+const DeliveryStatus = (props) => {
+  return (
+    <div>
+      <StatusChips />
+      <StartCancel />
+    </div>
+  );
+};
+
+export default DeliveryStatus;
