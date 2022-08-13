e354865ac38df027c49f255d7f7e8b17e4f017c4
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2022-08-13 14:33:26 -0400

    Altering font to Roboto

---
+++ src/components/UI/ChipMenu.js
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+import Chip from "./Chip"
+import classes from "./Chipnpm sxMenu.module.css";
+
+const ChipMenu = (props) => {
+
+    const [chipState, setChipState] = useState(undefined)
+
+    // use a ternary statement to conditionally apply an active / not active class to the pills.
+    const handleChipClick = (event) => {
+        event.preventDefault()
+        const eTarget = event.target.id
+        setChipState(eTarget)
+    }
+
+    const chipContents = props.array.map((item) => {
+        return <Chip id={item.id} name={item.name} key={item.id} onClick={handleChipClick} selected={chipState}/>;
+      });
+    
+  return <form>{chipContents}</form>;
+};
+
+export default ChipMenu
